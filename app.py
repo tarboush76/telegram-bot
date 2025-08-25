25da40dcb3d4f5b17fe662a3a72dc6658d2f7d9c
خالد طربوش سيف مقبل <tarboush76@yahoo.com>
2025-08-26 01:16:21 +0300

    app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ Flask شغال على Render!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
